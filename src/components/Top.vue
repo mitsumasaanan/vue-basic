21887f963ff11ab51ad909abdfa115f754ad1d22
mitsumasaanan <mitsumasa.anan449@gmail.com>
2021-10-27 13:58:25 +0900

    バリデーション実装

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -13,8 +13,16 @@
                         v-model="movieUrl"
                         placeholder="動画URLの【v= 】の後に続く英数字"
                        style="width: 300px"
-                   />
+                    />
                     {{ movieUrl }}
+                    <v-text-field
+						label="コメント"
+						v-model="comment"
+						:rules="commentRules"
+						placeholder="動画紹介のための任意のコメント"
+						class="mb-5"
+						style="width: 500px"
+					/>
                </v-form>
             </v-container>
         </v-card>
@@ -23,11 +31,36 @@
 
 <script>
     export default {
-       name: 'Top',
-       data () {
-           return {
-               movieUrl: "",
-           }
-       }
-   }
+        data (){
+            return {
+            movieUrl: "",
+                movieErrorMessages: [],
+                comment: "",
+                commentRules: [
+                    value => !!value || '入力してください',
+                    value => value.length <= 16 || '16文字以内で入力してください'
+                ],
+            }
+        },
+        watch: {
+          movieUrl: function (value) {
+            const stringValidation = /^[a-zA-Z0-9!-/:-@¥[-`{-~]+$/.test(value)
+            if (!value) {
+              this.movieErrorMessages = [
+                '入力してください',
+              ]
+            } else if (value.length !== 11) {
+              this.movieErrorMessages = [
+                '11文字で入力してください',
+              ]
+            } else if (!stringValidation) {
+              this.movieErrorMessages = [
+                '半角英数記号で入力してください',
+              ]
+            } else {
+              this.movieErrorMessages = []
+            }
+          }
+        }
+    }
 </script>
